9936566d9dbc97de3ac8efc3f5095bb8204e09fd
Pavel Alekseev <Kpotos139@gmail.com>
2021-02-01 12:31:55 +0300

    echo2

--- src/com/dom/pipeline/Args.groovy
+++ src/com/dom/pipeline/Args.groovy
@@ -8,7 +8,6 @@ class Args {
     }
 
     public void passArgs(String arg) {
-        echo "Pass Args test echo"
         script.sh """
             ls $arg .
         """
--- vars/passArg.groovy
+++ vars/passArg.groovy
@@ -2,6 +2,6 @@ import com.dom.pipeline.Args
 
 def call(String arg="") {
     Args args = new Args(this)
-
+    echo "Pass Args test echo"
     args.passArgs(arg)
 }

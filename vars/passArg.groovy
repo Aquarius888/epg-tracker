767b6f293492160773ae62cd29e1a2d2ab0ee312
Pavel Alekseev <Kpotos139@gmail.com>
2021-02-01 11:23:38 +0300

    typo

--- vars/passArg.groovy
+++ vars/passArg.groovy
@@ -2,7 +2,7 @@ import com.dom.pipeline.Args
 
 def call(String arg=null) {
     Args args = new Args(this)
-    if (arg=null) {
+    if (arg == null) {
         args('')
     } else {
         args(arg)}

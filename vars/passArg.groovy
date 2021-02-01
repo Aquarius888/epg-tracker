770f77e01f5e8cba784f6401729a715362067c4c
Pavel Alekseev <Kpotos139@gmail.com>
2021-02-01 12:06:19 +0300

    c

--- vars/passArg.groovy
+++ vars/passArg.groovy
@@ -3,10 +3,10 @@ import com.dom.pipeline.Args
 def call(String arg=".") {
     Args args = new Args(this)
     if (arg == ".") {
-        args(${arg})
+        args(arg)
     } else {
         arg += ' .'
-        args(${arg})
+        args(arg)
     }
-        
+
 }

b566abb9b28b79420ecd0376681235169d106121
Pavel Alekseev <Kpotos139@gmail.com>
2021-02-18 21:26:27 +0300

    test

--- vars/passArg.groovy
+++ vars/passArg.groovy
@@ -3,5 +3,6 @@ import com.dom.pipeline.Args
 def call(String arg="") {
     Args args = new Args(this)
     echo "Pass Args test echo"
-    echo args.passArgs(arg)
+    def output = args.passArgs(arg)
+    sh "cat $output | grep jenkins"
 }

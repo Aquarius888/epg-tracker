151cd068999bdd7ad5f4233fb2e30ef1c304d102
Pavel Alekseev <Kpotos139@gmail.com>
2021-02-21 13:29:18 +0300

    build user

--- vars/buildSetter.groovy
+++ vars/buildSetter.groovy
@@ -19,9 +19,13 @@ def call(String branch) {
         buildName "$BUILD_NUMBER-$ENVIRONMENT-$branch-$GIT_COMMIT_SHORT"
     }
     // USER = env.BUILD_USER ?: "Jenkins"
-    wrap([$class: 'BuildUser']){
-        USER = "${BUILD_USER}" ? "${BUILD_USER}" : "Jenkins"
-        buildDescription "Executed @ ${NODE_NAME}. Build started by ${BUILD_USER}"
-    }
 
+    try:
+        USER = "${BUILD_USER}"
+    catch Exception:
+        USER = "Jenkins"
+    wrap([$class: 'BuildUser']){
+            // USER = "${BUILD_USER}" ? "${BUILD_USER}" : "Jenkins"
+            buildDescription "Executed @ ${NODE_NAME}. Build started by ${BUILD_USER}"
+        }
 }

8399cb35402a257f3a5e3b96172b34a4be5e988b
Pavel Alekseev <Kpotos139@gmail.com>
2021-03-19 18:19:16 +0300

    Update passArg.groovy

--- vars/passArg.groovy
+++ vars/passArg.groovy
@@ -3,5 +3,10 @@ import com.dom.pipeline.Args
 def call(String arg="") {
     Args args = new Args(this)
     // echo "Pass Args test echo"
+    if (env.verbose) {
+       echo "Verbose" 
+    } else {
+       echo "FALSE"
+    }
     args.passArgs(arg)
 }
